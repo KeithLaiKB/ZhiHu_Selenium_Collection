55bef0233b7f51247f065785e64eb7428534fb4a
KeithLaiKB <laidelivery@163.com>
2017-05-17 18:06:41 +0800

    add personal info

--- JavaWebCrawlerBySeleniumByZhiHu/src/com/java/webcrawler/PersonalInfo.java
+++ JavaWebCrawlerBySeleniumByZhiHu/src/com/java/webcrawler/PersonalInfo.java
@@ -2,7 +2,7 @@ package com.java.webcrawler;
 
 public class PersonalInfo {
 	//你的账号
-	public static String account="l";
+	public static String account="lc";
 	//你的密码
 	public static String password="xxx";
 }
