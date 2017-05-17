1b0936b59a616e899f0affed75bd8ca9bf06479e
KeithLaiKB <laidelivery@163.com>
2017-05-17 17:53:45 +0800

    add person infor

--- JavaWebCrawlerBySeleniumByZhiHu/src/com/java/webcrawler/PersonInfo.java
+++ JavaWebCrawlerBySeleniumByZhiHu/src/com/java/webcrawler/PersonInfo.java
@@ -2,7 +2,7 @@ package com.java.webcrawler;
 
 public class PersonInfo {
 	//你的账号
-	public static String account="xxx";
+	public static String account="laidelivery@163.com";
 	//你的密码
-	public static String password="xxx";
+	public static String password="pc9394940";
 }
