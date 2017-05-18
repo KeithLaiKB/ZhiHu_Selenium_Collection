7c0c151437a2b67342be2c2d9b1d25717931deca
KeithLaiKB <laidelivery@163.com>
2017-05-18 12:38:14 +0800

    ok

--- JavaWebCrawlerBySeleniumByZhiHu/src/com/java/webcrawler/personalinfo/PersonalInfo.java
+++
@@ -1,9 +0,0 @@
-package com.java.webcrawler.personalinfo;
-
-public class PersonalInfo {
-	//你的账号
-	public static String account="gDASFADFg";
-	//你的密码
-	public static String password="ggggs";
-}
-  
