b8979a1f19fb92d4c78d303d67495632ce415f27
KeithLaiKB <laidelivery@163.com>
2017-05-18 11:59:45 +0800

    ok

--- JavaWebCrawlerBySeleniumByZhiHu/src/com/java/webcrawler/personalinfo/PersonalInfo.java
+++ JavaWebCrawlerBySeleniumByZhiHu/src/com/java/webcrawler/personalinfo/PersonalInfo.java
@@ -2,7 +2,7 @@ package com.java.webcrawler.personalinfo;
 
 public class PersonalInfo {
 	//你的账号
-	public static String account="xxx";
+	public static String account="xsdsfaxasdax";
 	//你的密码
 	public static String password="xxx";
 }
